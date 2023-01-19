d09d40eb3701668c8604dbf1c23d033671fb3448
Kamel R <kamel.r@wayne.edu>
2023-01-18 19:30:29 -0500

    Add project files.

---
+++ Employee.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <string>
+using namespace std;
+
+
+class Employee
+{
+private: 
+	string name;
+	string position;
+	string address; 
+	float salary; 
+
+
+public:
+
+	void setName(string n);
+	string getName();
+
+	void setPosition(string p);
+	string getPosition();
+
+	void setAddress(string a);
+	string getAddress();
+
+
+	void setSalary(string s);
+	float getSalary();
+
+
+	// constructors 
+	Employee();
+	Employee(string n, string p, string a, float s); 
+
+
+};
+
